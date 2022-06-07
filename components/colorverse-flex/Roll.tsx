e4f0d3a332b5159dd7109b96b052dfbf37933c2a
roylee0704 <roylee0704@gmail.com>
2022-06-07 13:15:40 +0700

    feat: add roll

--- components/colorverse-flex/Roll.tsx
+++ components/colorverse-flex/Roll.tsx
@@ -6,49 +6,43 @@ const Roll = () => {
   return (
     <div className="flex min-h-[calc(100vh-4.2rem)] flex-1 flex-col overflow-y-auto border-4 border-blue-500 bg-blue-100">
       <div className="relative h-full overflow-y-auto bg-yellow-500">
-        <div className="absolute">
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
-          <p>content.............</p>
+        <div className="absolute inset-x-0 top-0 z-50 border-2 border-red-700">
+          <p>Header</p>
+        </div>
+
+        <div className="absolute inset-0 z-40 overflow-y-auto border-2 border-black bg-green-400">
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
+          <p>Video</p>
         </div>
       </div>
     </div>
--- pages/colorverse-flex.tsx
+++ pages/colorverse-flex.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Chat from '../components/colorverse-flex/Chat'
 import Header from '../components/colorverse-flex/Header'
 import Main from '../components/colorverse-flex/Main'
+import Roll from '../components/colorverse-flex/Roll'
 import Side from '../components/colorverse-flex/Side'
 
 const ColorverseFlex = () => {
@@ -13,7 +14,7 @@ const ColorverseFlex = () => {
       {/* max-h-[0] */}
       <div className="mx-auto mt-[3.2rem] flex border-4 border-black md:space-x-5 lg:max-w-5xl">
         <Side />
-        <Chat />
+        <Roll />
       </div>
     </div>
   )
